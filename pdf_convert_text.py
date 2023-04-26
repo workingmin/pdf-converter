879899d10f035efef3512b0c47c2f25f7d503916
wangjianzheng <wangjianzheng@xhqb.com>
2023-04-26 11:54:12 +0800

    init

---
+++ pdf_convert_text.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("Usage: " + sys.argv[0] + " <PDF-file>")
+        sys.exit(0)
+    
+    pdf_file = sys.argv[1]
+
+    pages = convert_from_path(pdf_file)
+    for i, page in enumerate(pages):
+        page_file = "%d.png" % i
+        page.save(page_file, "png")
+        image = Image.open(page_file)
+        texts = pytesseract.image_to_string(image, lang='chi_sim')
+        text_file = "%d.txt" % i
+        with open(text_file, 'w') as f:
+            f.write(''.join(texts))
---
+++ pdf_convert_word.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import pdf2docx
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("Usage: " + sys.argv[0] + " <PDF-file> <output-folder>")
+        sys.exit(0)
+
+    pdf_file = sys.argv[1]
+    output_folder = sys.argv[2]
+    
+    docx_file = os.path.join(output_folder, os.path.splitext(os.path.basename(pdf_file))[0] + '.docx')
+    converter = pdf2docx.Converter(pdf_file)
+    converter.convert(docx_file, start=0, end=None)
+    converter.close()
+    
